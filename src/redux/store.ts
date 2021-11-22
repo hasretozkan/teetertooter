251b9b3581518764a4b60debc2ad1528020b59b3
Hasret Özkan <hasretozkan1@gmail.com>
2021-11-22 21:49:35 +0300

    formatted with prettier

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,14 +3,14 @@ import rootReducer from './reducer';
 import { configureStore } from '@reduxjs/toolkit';
 
 declare global {
-    interface Window {
-        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
 }
 
 export const store: Store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
--- src/redux/utils.ts
+++ src/redux/utils.ts
@@ -1,55 +1,64 @@
-import { MAX_ITEM_WEIGHT, HORIZONTAL_CELLS_COUNT, SCALE_AMOUNT, BASE_SCALE, CELL_UNIT_SIZE } from './constants';
+import {
+  MAX_ITEM_WEIGHT,
+  HORIZONTAL_CELLS_COUNT,
+  SCALE_AMOUNT,
+  BASE_SCALE,
+  CELL_UNIT_SIZE,
+} from './constants';
 import { FallingItemShape, MoveDirection } from './types';
 
 export const getRandomItemWeight = (): number => {
-    return Math.floor(Math.random() * MAX_ITEM_WEIGHT + 1);
+  return Math.floor(Math.random() * MAX_ITEM_WEIGHT + 1);
 };
 
 export const getRandomFallingItemShape = (): number => {
-    return Math.floor((Math.random() * Object.keys(FallingItemShape).length) / 2);
+  return Math.floor((Math.random() * Object.keys(FallingItemShape).length) / 2);
 };
 
 export const calculateOffset = (cellPosition: number): number => {
-    return CELL_UNIT_SIZE * cellPosition - CELL_UNIT_SIZE;
+  return CELL_UNIT_SIZE * cellPosition - CELL_UNIT_SIZE;
 };
 
 export const getRandomCellPositionX = (): number => {
-    return Math.floor(Math.random() * HORIZONTAL_CELLS_COUNT + 1);
+  return Math.floor(Math.random() * HORIZONTAL_CELLS_COUNT + 1);
 };
 
 export const calculateScaleSize = (weight: number): number => {
-    return BASE_SCALE + weight * SCALE_AMOUNT;
+  return BASE_SCALE + weight * SCALE_AMOUNT;
 };
 
 export const getHorizontalPositionAfterMove = (positionX: number, move: MoveDirection): number => {
-    switch (move) {
-        case MoveDirection.left:
-            return positionX - 1;
-        case MoveDirection.right:
-            return positionX + 1;
-        default:
-            return positionX;
-    }
+  switch (move) {
+    case MoveDirection.left:
+      return positionX - 1;
+    case MoveDirection.right:
+      return positionX + 1;
+    default:
+      return positionX;
+  }
 };
 
 export const getVerticalPositionAfterMove = (positionY: number, move: MoveDirection): number => {
-    switch (move) {
-        case MoveDirection.bottom:
-            return positionY + 1;
-        default:
-            return positionY;
-    }
+  switch (move) {
+    case MoveDirection.bottom:
+      return positionY + 1;
+    default:
+      return positionY;
+  }
 };
 
 export const calculateTorqueOfFallingItem = (mass: number, distance: number): number => {
-    return mass * distance;
+  return mass * distance;
 };
 
-export const reverseHorizontalCellPosition = (maxCount: number, currentPosition: number): number => {
-    return maxCount - currentPosition + 1;
+export const reverseHorizontalCellPosition = (
+  maxCount: number,
+  currentPosition: number
+): number => {
+  return maxCount - currentPosition + 1;
 };
 
 export const getRandomHexColor = (): string =>
-    `#${Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, '0')}`;
+  `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
